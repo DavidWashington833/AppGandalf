39b00d122d1b4042dc2a4533a6803cb7a1f9ee53
FELIPE ASSEM MICONI <felipe.amiconi@senacsp.edu.br>
2017-10-04 20:31:27 -0300

    Tela de Cadastro

---
+++ app/src/main/java/com/example/davidwfsilva/pi_4_appmobile/CrieSuaConta.java
@@ -0,0 +1,13 @@
+package com.example.davidwfsilva.pi_4_appmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrieSuaConta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crie_sua_conta);
+    }
+}
