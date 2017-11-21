620db4b497892c39f1e93d1d83cf59f162a23b39
Bruno <brunonunessantos919@gmail.com>
2017-11-20 22:53:52 -0200

    Tela de Checkout

---
+++ app/src/main/java/com/example/david/gandalf/CheckoutFragment02.java
@@ -0,0 +1,29 @@
+package com.example.david.gandalf;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CheckoutFragment02 extends Fragment {
+
+
+    public CheckoutFragment02() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_checkout_fragment02, container, false);
+    }
+
+}
