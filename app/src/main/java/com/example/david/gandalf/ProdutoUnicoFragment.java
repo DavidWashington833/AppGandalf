9f634dc90842571fcd84da0df5e83ba5158459b6
Igor Ramos <igor.ramos@live.com>
2017-11-24 22:58:27 -0200

    Integração de Endereço

--- app/src/main/java/com/example/david/gandalf/ProdutoUnicoFragment.java
+++ app/src/main/java/com/example/david/gandalf/ProdutoUnicoFragment.java
@@ -11,7 +11,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.david.gandalf.tasks.PegaProdutoUnicoTask;
-import com.example.david.gandalf.tasks.PegaProdutosTask;
 
 
 /**
