74be36cefd77493ff6a21607d92f6accec3898b0
Igor Ramos <igor.ramos@live.com>
2017-11-26 16:44:07 -0200

    Ação do Fazer Login do PermissaoFragment

--- app/src/main/java/com/storegandalf/david/gandalf/PermissaoFragment.java
+++ app/src/main/java/com/storegandalf/david/gandalf/PermissaoFragment.java
@@ -1,17 +1,23 @@
 package com.storegandalf.david.gandalf;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PermissaoFragment extends Fragment {
+
+    Button btnFazerLoginPermissao;
 
 
     public PermissaoFragment() {
@@ -23,7 +29,17 @@ public class PermissaoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_permissao, container, false);
+        View v  =  inflater.inflate(R.layout.fragment_permissao, container, false);
+
+        btnFazerLoginPermissao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newAct = new Intent(getContext(), LoginActivity.class);
+                startActivity(newAct);
+            }
+        });
+
+        return v;
     }
 
 }
